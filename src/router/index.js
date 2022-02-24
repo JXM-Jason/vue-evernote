6ab4b426d89a9176e4a624417b5bb42b8dee14f7
JXM <1162782002@qq.com>
2022-02-24 09:34:50 +0800

    Evernote-page

--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,7 @@ export default new Router({
       component: () => import("../components/Login.vue")
     },
     {
-      path: "/NotebookList",
+      path: "/",
       alias: "/NotebookList",
       name: "NotebookList",
       component: () => import("../components/NotebookList.vue")
